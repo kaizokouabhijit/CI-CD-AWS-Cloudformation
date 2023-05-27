eb52cfb614ec54926c02b66a15a2e7bf4206793c
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2023-05-27 06:14:42 +0530

    Add files via upload

---
+++ Java/docker_tag_push_image.sh
@@ -0,0 +1,13 @@
+main_image="my-main-image"
+app_image="my-app-image"
+
+docker rm main-container
+docker rm app-container
+echo "$?"
+docker build -t ${main_image} --build-arg target_image="Main" .
+docker build -t ${app_image} --build-arg target_image="App" .
+docker run -d --name main-container "$main_image"
+docker run -d --name app-container "$app_image"
+
+
+read -n 1 -s -r -p "Press any key to exit..."
