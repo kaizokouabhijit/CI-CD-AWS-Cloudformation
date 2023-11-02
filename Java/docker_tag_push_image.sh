60575adcbc12cddab332799b905bc427a646e350
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2023-11-02 10:12:06 +0530

    Update docker_tag_push_image.sh

--- Java/docker_tag_push_image.sh
+++ Java/docker_tag_push_image.sh
@@ -3,22 +3,12 @@ set -e
 main_image="my-$1-image"
 # app_image="my-app-image"
 
-container_name="main-container"
-
-# Check if the container exists
-if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
-    # Container exists, so remove it
-    docker rm "$container_name"
-    echo "Container $container_name removed."
-else
-    echo "Container $container_name does not exist."
-fi
 
 
 
 docker build -t ${main_image} --build-arg target_image="Main" .
 # docker build -t ${app_image} --build-arg target_image="App" .
-docker run -d --name main-container "$main_image"
+docker run -d --name main-$1-container "$main_image"
 # docker run -d --name app-container "$app_image"
 
 
