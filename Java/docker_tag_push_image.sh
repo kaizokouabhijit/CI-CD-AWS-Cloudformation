46cd9d52eaf1e5d5de315e9664687244ab0bb691
Abhijit Shubham <31901950+kaizokouabhijit@users.noreply.github.com>
2023-11-13 14:28:07 +0530

    Update docker_tag_push_image.sh

--- Java/docker_tag_push_image.sh
+++ Java/docker_tag_push_image.sh
@@ -1,15 +1,17 @@
-set -e
+# set -e
 
-main_image="my-$1-$2-image"
-# app_image="my-app-image"
+# main_image="my-$1-$2-image"
+# # app_image="my-app-image"
 
-# docker rm -f "/main-main-container"
-
-
-docker build -t ${main_image} --build-arg target_image="Main" .
-# docker build -t ${app_image} --build-arg target_image="App" .
-# docker run -d --name main-$1-$2-container "$main_image"
-# docker run -d --name app-container "$app_image"
+# # docker rm -f "/main-main-container"
 
 
-# read -n 1 -s -r -p "Press any key to exit..."
+# docker build -t ${main_image} --build-arg target_image="Main" .
+# # docker build -t ${app_image} --build-arg target_image="App" .
+# # docker run -d --name main-$1-$2-container "$main_image"
+# # docker run -d --name app-container "$app_image"
+
+
+# # read -n 1 -s -r -p "Press any key to exit..."
+
+docker build -t Javatestimage:testing .
